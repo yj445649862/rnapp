5cf7b0966ce0792597bd697a52028a702222f851
yj445649862 <417924273@qq.com>
2017-09-08 09:27:49 +0800

    统一入口

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by andy on 2017-09-08.
+ */
+import React from 'react'
+import {View,Text} from 'react-native'
+export default class Main extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <View>
+                <Text>老子是你亲爹</Text>
+            </View>
+        )
+    }
+}
