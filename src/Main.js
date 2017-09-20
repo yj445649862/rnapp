b15d1ab476bfc8bdd9f33d025744393a07279e90
andy <417924273@qq.com>
2017-09-20 14:39:41 +0800

    增加引导页

--- src/Main.js
+++ src/Main.js
@@ -2,16 +2,32 @@
  * Created by andy on 2017-09-08.
  */
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text,Image,StyleSheet} from 'react-native'
+import { Button } from 'antd-mobile';
+import ScreenFlash from './views/ScreenFlash'
 export default class Main extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
         return (
-            <View>
-                <Text>老子是你亲爹</Text>
+            <View style={{flex:1}}>
+                <ScreenFlash></ScreenFlash>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    backgroundImage1:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        width:null,
+        height:null,
+        //不加这句，就是按照屏幕高度自适应
+        //加上这几，就是按照屏幕自适应
+        //resizeMode:Image.resizeMode.contain,
+        //祛除内部元素的白色背景
+        backgroundColor:'rgba(0,0,0,0)',
+    }
+})
---
+++ src/views/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from 'antd-mobile'
+export default class  HomePage extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Button type="warning">wo shi dier ye</Button>
+        )
+    }
+}
